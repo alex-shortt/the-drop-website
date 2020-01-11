f1e73491e00df853258476da908a6a679355b6e4
Alex Shortt <alexander.shortt@gmail.com>
2020-01-10 23:40:01 -0800

    pipe data correctly

--- src/scenes/Drop/index.js
+++ src/scenes/Drop/index.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components/macro"
 
 import Helmet from "components/Helmet"
+import { getDrop } from "services/firebase"
 
 export default function Drop(props) {
   const {
@@ -10,8 +11,44 @@ export default function Drop(props) {
     }
   } = props
 
+  const [error, setError] = useState(false)
+  const [drop, setDrop] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getDrop(id)
+      if (!result) {
+        setError("Drop Not Found")
+      } else {
+        setDrop(result)
+      }
+    }
+
+    if (!drop && !error) {
+      fetchData()
+    }
+  }, [drop, error, id])
+
   console.log(id)
   console.log(venmo)
+
+  if (!drop && !error) {
+    return (
+      <>
+        <Helmet title="Drop" />
+        Loading...
+      </>
+    )
+  }
+
+  if (error) {
+    return (
+      <>
+        <Helmet title="Drop" />
+        Error: {error}
+      </>
+    )
+  }
 
   return (
     <>
