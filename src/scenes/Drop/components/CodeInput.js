2a63b89278621f357fcbcf171aba020714c6d012
Alex Shortt <alexander.shortt@gmail.com>
2020-01-11 21:16:01 -0800

    style qr reader

--- src/scenes/Drop/components/CodeInput.js
+++ src/scenes/Drop/components/CodeInput.js
@@ -1,32 +1,48 @@
 import React, { useState, useCallback } from "react"
 import styled from "styled-components"
 import QrReaderBase from "react-qr-reader"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const Container = styled.div`
+const Button = styled.button`
   position: absolute;
   bottom: 10vh;
   left: 50%;
   transform: translateX(-50%);
-  background: white;
-  padding: 20px;
-  display: flex;
-`
+  background: #2c6674;
+  border: none;
+  font-size: 1.8rem;
+  padding: 15px 25px;
+  color: white;
+  max-width: 250px;
+  width: 100%;
+  border-radius: 10px;
+  z-index: 2;
 
-const Input = styled.input`
-  margin-right: 20px;
-`
-
-const Button = styled.button`
-  border: 1px solid black;
-  background: white;
+  @media screen and (max-width: 700px) {
+    font-size: 1.4rem;
+    max-width: 200px;
+  }
 `
 
 const QrReader = styled(QrReaderBase)`
   position: absolute;
-  top: 30px;
+  top: 50%;
   left: 50%;
-  transform: translateX(-50%);
-  width: 50vh !important;
+  transform: translateX(-50%) translateY(-50%);
+  width: 500px !important;
+  height: 500px;
+  z-index: 2;
+`
+
+const Background = styled.div`
+  background: black;
+  opacity: 0.7;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 `
 
 export default function CodeInput(props) {
@@ -52,19 +68,25 @@ export default function CodeInput(props) {
   return (
     <>
       {qrOpen && (
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: "100%" }}
-        />
+        <>
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: "100%" }}
+          />
+          <Background />
+        </>
       )}
-      <Container>
-        <Input value={state} onChange={e => setState(e.target.value)} />
-        <Button onClick={toggleQrOpen}>
-          {qrOpen ? "Close" : "Open"} Camera
-        </Button>
-      </Container>
+      <Button onClick={toggleQrOpen}>
+        {qrOpen ? (
+          <FontAwesomeIcon icon="times" />
+        ) : (
+          <>
+            <FontAwesomeIcon icon="camera" /> Submit
+          </>
+        )}
+      </Button>
     </>
   )
 }
--- src/styles/fontawesome.js
+++ src/styles/fontawesome.js
@@ -1,8 +1,15 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faUsers, faMapMarker } from "@fortawesome/free-solid-svg-icons"
+import {
+  faUsers,
+  faMapMarker,
+  faCamera,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 
 library.add(faUsers)
 library.add(faMapMarker)
+library.add(faCamera)
+library.add(faTimes)
 
 library.add(fab)
